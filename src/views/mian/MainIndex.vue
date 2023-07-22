af98b9f0a12357ca487383c0157416e3f75bb5ff
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-07-22 23:00:35 +0800

    main page build

--- src/views/mian/MainIndex.vue
+++ src/views/mian/MainIndex.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="mian-index">
+    <el-container class="main-container">
+      <el-aside width="200px">
+        <main-aside />
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
+import MainAside from './cpns/MainAside.vue'
 </script>
 
 <style scoped lang="less">
-
+.mian-index,
+.main-container {
+  height: 100%;
+}
 </style>
