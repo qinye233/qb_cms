71fcc64907d58efaae4f301a2aff53f4152ff121
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-09-03 16:39:22 +0800

    :bug: fix: 文件引用问题

--- src/views/404/NotFound.vue
+++ src/views/404/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="not-found">
-    <img src="src\assets\img\v2-ec9ed225d1cb969de540cd1dcb449c2c_r.png" alt="404" />
+    <img src="../../assets/img/v2-ec9ed225d1cb969de540cd1dcb449c2c_r.png" alt="404" />
   </div>
 </template>
 
